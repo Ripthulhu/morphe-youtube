b92f34522f69f784efc6c98a52264c16e35c4a1a
Dylan <70382811+Ripthulhu@users.noreply.github.com>
2026-07-17 20:00:24 +0200

    fix(keep): preserve original package name

--- patches/src/main/kotlin/app/morphe/patches/keep/misc/gms/GmsCoreSupportPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/keep/misc/gms/GmsCoreSupportPatch.kt
@@ -3,14 +3,13 @@ package app.morphe.patches.keep.misc.gms
 import app.morphe.patches.keep.misc.extension.sharedExtensionPatch
 import app.morphe.patches.keep.misc.gms.Constants.KEEP_PACKAGE_NAME
 import app.morphe.patches.keep.misc.gms.Constants.KEEP_PACKAGE_SIGNATURE
-import app.morphe.patches.keep.misc.gms.Constants.MORPHE_KEEP_PACKAGE_NAME
 import app.morphe.patches.keep.shared.Constants.COMPATIBILITY_KEEP
 import app.morphe.patches.shared.misc.gms.gmsCoreSupportPatch
 
 @Suppress("unused")
 val gmsCoreSupportPatch = gmsCoreSupportPatch(
     fromPackageName = KEEP_PACKAGE_NAME,
-    toPackageNameDefault = MORPHE_KEEP_PACKAGE_NAME,
+    toPackageNameDefault = KEEP_PACKAGE_NAME,
     extensionPatch = sharedExtensionPatch,
     gmsCoreSupportResourcePatchFactory = ::gmsCoreSupportResourcePatch,
 ) {
@@ -20,7 +19,7 @@ val gmsCoreSupportPatch = gmsCoreSupportPatch(
 private fun gmsCoreSupportResourcePatch() =
     app.morphe.patches.shared.misc.gms.gmsCoreSupportResourcePatch(
         fromPackageName = KEEP_PACKAGE_NAME,
-        toPackageNameDefault = MORPHE_KEEP_PACKAGE_NAME,
+        toPackageNameDefault = KEEP_PACKAGE_NAME,
         spoofedPackageSignature = KEEP_PACKAGE_SIGNATURE,
         screen = null,
     )
